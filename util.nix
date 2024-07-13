690486cc7f790980d5d72c66d8b39426e27f6acf
Manuel Brea <m.brea.carreras@gmail.com>
2024-07-13 21:02:06 +0100

    Add css color fn

--- util.nix
+++ util.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ ... }: rec {
   fromBase16 = { palette, ... }: rec {
     color0 = palette.base00;
     color1 = palette.base08;
@@ -40,4 +40,30 @@
     builtins.mapAttrs (key: value:
       if key == "fg" || key == "bg" || key == "sp" then "#${value}" else value)
     hl);
+
+  toGtkCss = { palette, ... }:
+    let
+      colors = fromBase16 { inherit palette; };
+      # css
+    in ''
+      @define-color foreground ${colors.foreground};
+      @define-color background ${colors.background};
+
+      @define-color color0  ${colors.color0};
+      @define-color color1  ${colors.color1};
+      @define-color color2  ${colors.color2};
+      @define-color color3  ${colors.color3};
+      @define-color color4  ${colors.color4};
+      @define-color color5  ${colors.color5};
+      @define-color color6  ${colors.color6};
+      @define-color color7  ${colors.color7};
+      @define-color color8  ${colors.color8};
+      @define-color color9  ${colors.color9};
+      @define-color color10 ${colors.color10};
+      @define-color color11 ${colors.color11};
+      @define-color color12 ${colors.color12};
+      @define-color color13 ${colors.color13};
+      @define-color color14 ${colors.color14};
+      @define-color color15 ${colors.color15};
+    '';
 }

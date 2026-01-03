7b3c3fa00e7a0b29adae1aee2159c8bcba45153a
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-03 19:32:58 +0000

    fix

--- variants/rgb.nix
+++ variants/rgb.nix
@@ -1,8 +1,8 @@
 { ... }:
 {
   mix = ratio: left: right: {
-    r = builtins.floor ((1.0 - ratio) * left.r + ratio * right.r);
-    g = builtins.floor ((1.0 - ratio) * left.g + ratio * right.g);
-    b = builtins.floor ((1.0 - ratio) * left.b + ratio * right.b);
+    r = (1.0 - ratio) * left.r + ratio * right.r;
+    g = (1.0 - ratio) * left.g + ratio * right.g;
+    b = (1.0 - ratio) * left.b + ratio * right.b;
   };
 }
